fcdba3b1feab224566650cf79b4d36da032fb4b1
surrealtm <unreal.hiraeth@gmail.com>
2024-05-01 23:01:33 +0200

    Updated foundation.

--- Code/Core/src/tests.cpp
+++ Code/Core/src/tests.cpp
@@ -4,8 +4,13 @@
 #include "../src/strings.h"
 
 int main() {
+#if FOUNDATION_DEVELOPER
     World_Handle world = core_do_large_volumes_test(false);
     core_print_memory_information(world);
     core_destroy_world(world);
+#else
+    World_Handle world = core_create_world(100, 10, 100);
+    core_destroy_world(world);
+#endif
     return 0;
 }
